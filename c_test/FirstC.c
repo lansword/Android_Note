80b07d02d27e01c5f9c2f0bad2362628106af675
blue-sword <1987039055@qq.com>
2018-11-06 00:36:21 +0800

    +1

--- c_test/FirstC.c
+++ c_test/FirstC.c
@@ -2,5 +2,20 @@
 
 int main()
 {
-	printf("hello world\n");
+	float fahr,celsius;
+	int lower,upper,step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+  	fahr = lower;
+  	while(fahr <= upper){
+  		celsius = 5 *(fahr-32)/9;
+  		printf("%3.0f\t%6.1f\n",fahr,celsius);
+  		fahr+=step;
+  	}
 }
+
+
+
